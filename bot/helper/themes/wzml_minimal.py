e7f7266bc117e75f0e3b6da60b6294ece0b44051
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2023-08-26 09:22:04 +0600

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -2,77 +2,88 @@
 class WZMLStyle:
     # ----------------------
     # async def start(client, message) ---> __main__.py
-    ST_BN1_NAME = 'Repo'
-    ST_BN1_URL = 'https://www.github.com/weebzone/WZML-X'
-    ST_BN2_NAME = 'Updates'
-    ST_BN2_URL = 'https://t.me/WZML_X'
+    ST_BN1_NAME = '🔥 MASTER 🔥'
+    ST_BN1_URL = 'https://t.me/nafisfuad1'
+    ST_BN2_NAME = '♦️ GROUP ♦️'
+    ST_BN2_URL = 'https://t.me/gdrive_mirror'
     ST_MSG = '''<i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>
-<b>Type {help_command} to get a list of available commands</b>'''
-    ST_BOTPM = '''<i>Now, This bot will send all your files and links here. Start Using ...</i>'''
-    ST_UNAUTH = '''<i>You Are not authorized user! Deploy your own WZML-X Mirror-Leech bot</i>'''
+<b>Type {help_command} to get a list of available commands</b>\n MADE WITH ❤️ BY @Nafisfuad1'''
+    ST_BOTPM = '''<i>Now, This bot will send all your files and links here. Start Using ...\n MADE WITH ❤️ BY @Nafisfuad1</i>'''
+    ST_UNAUTH = '''<i>You Are not authorized user!\n ASK >> @Nafisfuad1</i>'''
     # ---------------------
 
     # async def stats(client, message):
     BOT_STATS = '''⌬ <b><i>BOT STATISTICS :</i></b>
-┖ <b>Bot Uptime :</b> {bot_uptime}
+༶•┈┈┈┈┈┈┈┈┈୨💤୧┈┈┈┈┈┈┈┈┈•༶
+✦ <b>Bot Uptime :</b> {bot_uptime}
+༶•┈┈┈┈┈┈┈┈┈୨💤୧┈┈┈┈┈┈┈┈┈•༶
 
-┎ <b><i>RAM ( MEMORY ) :</i></b>
-┃ {ram_bar} {ram}%
-┖ <b>U :</b> {ram_u} | <b>F :</b> {ram_f} | <b>T :</b> {ram_t}
+༶•┈┈┈┈┈┈┈┈┈୨⚡️୧┈┈┈┈┈┈┈┈┈•༶
+✦ <b><i>RAM ( MEMORY ) :</i></b>
+✦ {ram_bar} {ram}%
+✦ <b>Used :</b> {ram_u} | <b>Free :</b> {ram_f}
+✦ <b>Total :</b> {ram_t}
+༶•┈┈┈┈┈┈┈┈┈୨⚡️୧┈┈┈┈┈┈┈┈┈•༶
 
-┎ <b><i>SWAP MEMORY :</i></b>
-┃ {swap_bar} {swap}%
-┖ <b>U :</b> {swap_u} | <b>F :</b> {swap_f} | <b>T :</b> {swap_t}
+༶•┈┈┈┈┈┈┈┈┈୨🔑୧┈┈┈┈┈┈┈┈┈•༶
+✦ <b><i>SWAP MEMORY :</i></b>
+✦ {swap_bar} {swap}%
+✦ <b>U :</b> {swap_u} | <b>F :</b> {swap_f} | <b>T :</b> {swap_t}
+༶•┈┈┈┈┈┈┈┈┈୨🔑୧┈┈┈┈┈┈┈┈┈•༶
 
-┎ <b><i>DISK :</i></b>
-┃ {disk_bar} {disk}%
-┃ <b>Total Disk Read :</b> {disk_read}
-┃ <b>Total Disk Write :</b> {disk_write}
-┖ <b>U :</b> {disk_u} | <b>F :</b> {disk_f} | <b>T :</b> {disk_t}
+༶•┈┈┈┈┈┈┈┈┈୨📀୧┈┈┈┈┈┈┈┈┈•༶
+✦ <b><i>DISK :</i></b>
+✦ {disk_bar} {disk}%
+✦ <b>Total Disk Read :</b> {disk_read}
+✦ <b>Total Disk Write :</b> {disk_write}
+✦ <b>U :</b> {disk_u} | <b>F :</b> {disk_f} | <b>T :</b> {disk_t}
+༶•┈┈┈┈┈┈┈┈┈୨📀୧┈┈┈┈┈┈┈┈┈•༶
+
+✦ Made With ❤️ BY @Nafisfuad1
     
     '''
     SYS_STATS = '''⌬ <b><i>OS SYSTEM :</i></b>
-┠ <b>OS Uptime :</b> {os_uptime}
-┠ <b>OS Version :</b> {os_version}
-┖ <b>OS Arch :</b> {os_arch}
+✦ <b>OS Uptime :</b> {os_uptime}
+✦ <b>OS Version :</b> {os_version}
+✦ <b>OS Arch :</b> {os_arch}
 
 ⌬ <b><i>NETWORK STATS :</i></b>
-┠ <b>Upload Data:</b> {up_data}
-┠ <b>Download Data:</b> {dl_data}
-┠ <b>Pkts Sent:</b> {pkt_sent}k
-┠ <b>Pkts Received:</b> {pkt_recv}k
-┖ <b>Total I/O Data:</b> {tl_data}
+✦ <b>Upload Data:</b> {up_data}
+✦ <b>Download Data:</b> {dl_data}
+✦ <b>Pkts Sent:</b> {pkt_sent}k
+✦ <b>Pkts Received:</b> {pkt_recv}k
+✦ <b>Total I/O Data:</b> {tl_data}
 
-┎ <b>CPU :</b>
-┃ {cpu_bar} {cpu}%
-┠ <b>CPU Frequency :</b> {cpu_freq}
-┠ <b>System Avg Load :</b> {sys_load}
-┠ <b>P-Core(s) :</b> {p_core} | <b>V-Core(s) :</b> {v_core}
-┠ <b>Total Core(s) :</b> {total_core}
-┖ <b>Usable CPU(s) :</b> {cpu_use}
+✦ <b>CPU :</b>
+✦ {cpu_bar} {cpu}%
+✦ <b>CPU Frequency :</b> {cpu_freq}
+✦ <b>System Avg Load :</b> {sys_load}
+✦ <b>P-Core(s) :</b> {p_core} | <b>V-Core(s) :</b> {v_core}
+✦ <b>Total Core(s) :</b> {total_core}
+✦ <b>Usable CPU(s) :</b> {cpu_use}
     '''
     REPO_STATS = '''⌬ <b><i>REPO STATISTICS :</i></b>
-┠ <b>Bot Updated :</b> {last_commit}
-┠ <b>Current Version :</b> {bot_version}
-┠ <b>Latest Version :</b> {lat_version}
-┖ <b>Last ChangeLog :</b> {commit_details}
+✦ <b>Bot Updated :</b> {last_commit}
+✦ <b>Current Version :</b> {bot_version}
+✦ <b>Latest Version :</b> {lat_version}
+✦ <b>Master :</b> @Nafisfuad1
 
 ⌬ <b>REMARKS :</b> <code>{remarks}</code>
     '''
     BOT_LIMITS = '''⌬ <b><i>BOT LIMITATIONS :</i></b>
-┠ <b>Direct Limit :</b> {DL} GB
-┠ <b>Torrent Limit :</b> {TL} GB
-┠ <b>GDrive Limit :</b> {GL} GB
-┠ <b>YT-DLP Limit :</b> {YL} GB
-┠ <b>Playlist Limit :</b> {PL}
-┠ <b>Mega Limit :</b> {ML} GB
-┠ <b>Clone Limit :</b> {CL} GB
-┖ <b>Leech Limit :</b> {LL} GB
+✦ <b>Direct Limit :</b> {DL} GB
+✦ <b>Torrent Limit :</b> {TL} GB
+✦ <b>GDrive Limit :</b> {GL} GB
+✦ <b>YT-DLP Limit :</b> {YL} GB
+✦ <b>Playlist Limit :</b> {PL}
+✦ <b>Mega Limit :</b> {ML} GB
+✦ <b>Clone Limit :</b> {CL} GB
+✦ <b>Leech Limit :</b> {LL} GB
 
-┎ <b>Token Validity :</b> {TV}
-┠ <b>User Time Limit :</b> {UTI} / task
-┠ <b>User Parallel Tasks :</b> {UT}
-┖ <b>Bot Parallel Tasks :</b> {BT}
+✦ <b>Token Validity :</b> {TV}
+✦ <b>User Time Limit :</b> {UTI} / task
+✦ <b>User Parallel Tasks :</b> {UT}
+✦ <b>Bot Parallel Tasks :</b> {BT}
     '''
     # ---------------------
 
@@ -82,24 +93,24 @@ class WZMLStyle:
 
     # async def restart_notification(): ---> __main__.py
     RESTART_SUCCESS = '''⌬ <b><i>Restarted Successfully!</i></b>
-┠ <b>Date:</b> {date}
-┠ <b>Time:</b> {time}
-┠ <b>TimeZone:</b> {timz}
-┖ <b>Version:</b> {version}'''
+❒ <b>Date:</b> {date}
+❒ <b>Time:</b> {time}
+❒ <b>TimeZone:</b> {timz}
+❒ <b>Version:</b> {version}'''
     RESTARTED = '''⌬ <b><i>Bot Restarted!</i></b>'''
     # ---------------------
 
     # async def ping(client, message): ---> __main__.py
-    PING = '<i>Starting Ping..</i>'
+    PING = '<i>❒ Starting Ping..</i>'
     PING_VALUE = '<b>Pong</b>\n<code>{value} ms..</code>'
     # ---------------------
 
     # async def onDownloadStart(self): --> tasks_listener.py
     LINKS_START = """<b><i>Task Started</i></b>
-┠ <b>Mode:</b> {Mode}
-┖ <b>By:</b> {Tag}\n\n"""
+❒ <b>Mode:</b> {Mode}
+❒ <b>By:</b> {Tag}\n\n"""
     LINKS_SOURCE = """➲ <b>Source:</b>
-┖ <b>Added On:</b> {On}
+❒ <b>Added On:</b> {On}
 ------------------------------------------
 {Source}
 ------------------------------------------\n\n"""
@@ -109,25 +120,25 @@ class WZMLStyle:
     L_LOG_START =           "➲ <b><u>Leech Started :</u></b>\n┃\n┠ <b>User :</b> {mention} ( #ID{uid} )\n┖ <b>Source :</b> <a href='{msg_link}'>Click Here</a>"
 
     # async def onUploadComplete(): ---> tasks_listener.py
-    NAME =                  '<b><i>{Name}</i></b>\n┃\n'
-    SIZE =                  '┠ <b>Size: </b>{Size}\n'
-    ELAPSE =                '┠ <b>Elapsed: </b>{Time}\n'
-    MODE =                  '┠ <b>Mode: </b>{Mode}\n'
+    NAME =                  '≫🗂  <b><i>{Name}</i></b>\n┃\n'
+    SIZE =                  '≫📐 <b>Size: </b>{Size}\n'
+    ELAPSE =                '≫⌛️ <b>It Took: </b>{Time}\n'
+    MODE =                  '≫⚙️ <b>Mode: </b>{Mode}\n'
 
     # ----- LEECH -------
-    L_TOTAL_FILES =         '┠ <b>Total Files: </b>{Files}\n'
-    L_CORRUPTED_FILES =     '┠ <b>Corrupted Files: </b>{Corrupt}\n'
-    L_CC =                  '┖ <b>By: </b>{Tag}\n\n'
+    L_TOTAL_FILES =         '≫ 🔄️  <b>Total Files: </b>{Files}\n'
+    L_CORRUPTED_FILES =     '≫ ❌  <b>Corrupted Files: </b>{Corrupt}\n'
+    L_CC =                  '≫ 👤 <b>User: </b>{Tag}\n\n'
     PM_BOT_MSG =            '➲ <b><i>File(s) have been Sent above</i></b>'
     L_BOT_MSG =             '➲ <b><i>File(s) have been Sent to Bot PM (Private)</i></b>'
     L_LL_MSG =              '➲ <b><i>File(s) have been Sent. Access via Links...</i></b>'
     
     # ----- MIRROR -------
-    M_TYPE =                '┠ <b>Type: </b>{Mimetype}\n'
-    M_SUBFOLD =             '┠ <b>SubFolders: </b>{Folder}\n'
-    TOTAL_FILES =           '┠ <b>Files: </b>{Files}\n'
-    RCPATH =                '┠ <b>Path: </b><code>{RCpath}</code>\n'
-    M_CC =                  '┖ <b>By: </b>{Tag}\n\n'
+    M_TYPE =                '≫ 🗃️ <b>Type: </b>{Mimetype}\n'
+    M_SUBFOLD =             '≫ 🗂 <b>SubFolders: </b>{Folder}\n'
+    TOTAL_FILES =           '≫ 🔄️ <b>Files: </b>{Files}\n'
+    RCPATH =                '≫ 🍁 <b>Path: </b><code>{RCpath}</code>\n'
+    M_CC =                  '≫ 👤 <b>User: </b>{Tag}\n\n'
     M_BOT_MSG =             '➲ <b><i>Link(s) have been Sent to Bot PM (Private)</i></b>'
     
     # ----- BUTTONS -------
@@ -146,47 +157,47 @@ class WZMLStyle:
 
     # def get_readable_message(): ---> bot_utilis.py
     ####--------OVERALL MSG HEADER----------
-    STATUS_NAME =       '<b><i>{Name}</i></b>'
+    STATUS_NAME =       '≫ ⚙️ <b><i>{Name}</i></b>'
 
     #####---------PROGRESSIVE STATUS-------
-    BAR =               '\n┃ {Bar}'
-    PROCESSED =         '\n┠ <b>Processed:</b> {Processed}'
-    STATUS =            '\n┠ <b>Status:</b> <a href="{Url}">{Status}</a>'
+    BAR =               '\n❒ {Bar}'
+    PROCESSED =         '\n❒ <b>Processed:</b> {Processed}'
+    STATUS =            '\n❒ <b>Status:</b> <a href="{Url}">{Status}</a>'
     ETA =                                                ' | <b>ETA:</b> {Eta}'
-    SPEED =             '\n┠ <b>Speed:</b> {Speed}'
+    SPEED =             '\n❒ <b>Speed:</b> {Speed}'
     ELAPSED =                                     ' | <b>Elapsed:</b> {Elapsed}'
-    ENGINE =            '\n┠ <b>Engine:</b> {Engine}'
-    STA_MODE =          '\n┠ <b>Mode:</b> {Mode}'
-    SEEDERS =           '\n┠ <b>Seeders:</b> {Seeders} | '
+    ENGINE =            '\n❒ <b>Engine:</b> {Engine}'
+    STA_MODE =          '\n❒ <b>Mode:</b> {Mode}'
+    SEEDERS =           '\n❒ <b>Seeders:</b> {Seeders} | '
     LEECHERS =                                           '<b>Leechers:</b> {Leechers}'
 
     ####--------SEEDING----------
-    SEED_SIZE =      '\n┠ <b>Size: </b>{Size}'
-    SEED_SPEED =     '\n┠ <b>Speed: </b> {Speed} | '
+    SEED_SIZE =      '\n❒ <b>Size: </b>{Size}'
+    SEED_SPEED =     '\n❒ <b>Speed: </b> {Speed} | '
     UPLOADED =                                     '<b>Uploaded: </b> {Upload}'
-    RATIO =          '\n┠ <b>Ratio: </b> {Ratio} | '
+    RATIO =          '\n❒ <b>Ratio: </b> {Ratio} | '
     TIME =                                         '<b>Time: </b> {Time}'
-    SEED_ENGINE =    '\n┠ <b>Engine:</b> {Engine}'
+    SEED_ENGINE =    '\n❒ <b>Engine:</b> {Engine}'
 
     ####--------NON-PROGRESSIVE + NON SEEDING----------
-    STATUS_SIZE =    '\n┠ <b>Size: </b>{Size}'
-    NON_ENGINE =     '\n┠ <b>Engine:</b> {Engine}'
+    STATUS_SIZE =    '\n❒ <b>Size: </b>{Size}'
+    NON_ENGINE =     '\n❒ <b>Engine:</b> {Engine}'
 
     ####--------OVERALL MSG FOOTER----------
-    USER =              '\n┠ <b>User:</b> <code>{User}</code> | '
+    USER =              '\n❒ <b>User:</b> <code>{User}</code> | '
     ID =                                                        '<b>ID:</b> <code>{Id}</code>'
-    BTSEL =          '\n┠ <b>Select:</b> {Btsel}'
-    CANCEL =         '\n┖ {Cancel}\n\n'
+    BTSEL =          '\n❒ <b>Select:</b> {Btsel}'
+    CANCEL =         '\n❒ {Cancel}\n\n'
 
     ####------FOOTER--------
     FOOTER = '⌬ <b><i>Bot Stats</i></b>\n'
-    TASKS =  '┠ <b>Tasks:</b> {Tasks}\n'
-    BOT_TASKS = '┠ <b>Tasks:</b> {Tasks}/{Ttask} | <b>AVL:</b> {Free}\n'
-    Cpu = '┠ <b>CPU:</b> {cpu}% | '
+    TASKS =  '▼ <b>Tasks:</b> {Tasks}\n'
+    BOT_TASKS = '▼ <b>Tasks:</b> {Tasks}/{Ttask} | <b>AVL:</b> {Free}\n'
+    Cpu = '▼ <b>CPU:</b> {cpu}% | '
     FREE =                      '<b>F:</b> {free} [{free_p}%]'
-    Ram = '\n┠ <b>RAM:</b> {ram}% | '
+    Ram = '\n▼ <b>RAM:</b> {ram}% | '
     uptime =                     '<b>UPTIME:</b> {uptime}'
-    DL = '\n┖ <b>DL:</b> {DL}/s | '
+    DL = '\n▼ <b>DL:</b> {DL}/s | '
     UL =                        '<b>UL:</b> {UL}/s'
 
     ###--------BUTTONS-------
@@ -200,17 +211,17 @@ class WZMLStyle:
     # ---------------------
 
     # async def countNode(_, message): ----> gd_count.py
-    COUNT_MSG = '<b>Counting:</b> <code>{LINK}</code>'
-    COUNT_NAME = '<b><i>{COUNT_NAME}</i></b>\n┃\n'
-    COUNT_SIZE = '┠ <b>Size: </b>{COUNT_SIZE}\n'
-    COUNT_TYPE = '┠ <b>Type: </b>{COUNT_TYPE}\n'
-    COUNT_SUB =  '┠ <b>SubFolders: </b>{COUNT_SUB}\n'
-    COUNT_FILE = '┠ <b>Files: </b>{COUNT_FILE}\n'
-    COUNT_CC =   '┖ <b>By: </b>{COUNT_CC}\n'
+    COUNT_MSG = '≫ 🗂 <b>Counting:</b> <code>{LINK}</code>'
+    COUNT_NAME = '≫ 🗃️ <b><i>{COUNT_NAME}</i></b>\n┃\n'
+    COUNT_SIZE = '❒ <b>Size: </b>{COUNT_SIZE}\n'
+    COUNT_TYPE = '❒ <b>Type: </b>{COUNT_TYPE}\n'
+    COUNT_SUB =  '❒ <b>SubFolders: </b>{COUNT_SUB}\n'
+    COUNT_FILE = '❒ <b>Files: </b>{COUNT_FILE}\n'
+    COUNT_CC =   '❒ <b>By: </b>{COUNT_CC}\n'
     # ---------------------
 
     # LIST ---> gd_list.py
-    LIST_SEARCHING = '<b>Searching for <i>{NAME}</i></b>'
+    LIST_SEARCHING = '<b>❒ Searching for <i>{NAME}</i></b>'
     LIST_FOUND = '<b>Found {NO} result for <i>{NAME}</i></b>'
     LIST_NOT_FOUND = 'No result found for <i>{NAME}</i>'
     # ---------------------
@@ -219,49 +230,49 @@ class WZMLStyle:
     NO_ACTIVE_DL = '''<i>No Active Downloads!</i>
     
 ⌬ <b><i>Bot Stats</i></b>
-┠ <b>CPU:</b> {cpu}% | <b>F:</b> {free} [{free_p}%]
-┖ <b>RAM:</b> {ram} | <b>UPTIME:</b> {uptime}
+▼ <b>CPU:</b> {cpu}% | <b>F:</b> {free} [{free_p}%]
+▼ <b>RAM:</b> {ram} | <b>UPTIME:</b> {uptime}
     '''
     # ---------------------
 
     # USER Setting --> user_setting.py 
     USER_SETTING = '''㊂ <b><u>User Settings :</u></b>
         
-┎<b> Name :</b> {NAME} ( <code>{ID}</code> )
-┠<b> Username :</b> {USERNAME}
-┠<b> Telegram DC :</b> {DC}
-┖<b> Language :</b> {LANG}'''
+❒ <b> Name :</b> {NAME} ( <code>{ID}</code> )
+❒ <b> Username :</b> {USERNAME}
+❒ <b> Telegram DC :</b> {DC}
+❒ <b> Language :</b> {LANG}'''
 
     UNIVERSAL = '''㊂ <b><u>Universal Settings : {NAME}</u></b>
 
-┎<b> YT-DLP Options :</b> <b><code>{YT}</code></b>
-┠<b> Daily Tasks :</b> <code>{DT}</code> per day
-┠<b> Last Bot Used :</b> <code>{LAST_USED}</code>
-┠<b> MediaInfo Mode :</b> <code>{MEDIAINFO}</code>
-┠<b> Save Mode :</b> <code>{SAVE_MODE}</code>
-┖<b> User Bot PM :</b> <code>{BOT_PM}</code>'''
+❒ <b> YT-DLP Options :</b> <b><code>{YT}</code></b>
+❒ <b> Daily Tasks :</b> <code>{DT}</code> per day
+❒ <b> Last Bot Used :</b> <code>{LAST_USED}</code>
+❒ <b> MediaInfo Mode :</b> <code>{MEDIAINFO}</code>
+❒ <b> Save Mode :</b> <code>{SAVE_MODE}</code>
+❒ <b> User Bot PM :</b> <code>{BOT_PM}</code>'''
 
     MIRROR = '''㊂ <b><u>Mirror/Clone Settings : {NAME}</u></b>
 
-┎<b> RClone Config :</b> <i>{RCLONE}</i>
-┠<b> Mirror Prefix :</b> <code>{MPREFIX}</code>
-┠<b> Mirror Suffix :</b> <code>{MSUFFIX}</code>
-┠<b> Mirror Remname :</b> <code>{MREMNAME}</code>
-┠<b> DDL Server(s) :</b> <i>{DDL_SERVER}</i>
-┠<b> User TD Mode :</b> <i>{TMODE}</i>
-┠<b> Total User TD(s) :</b> <i>{USERTD}</i>
-┖<b> Daily Mirror :</b> <code>{DM}</code> per day'''
+❒ <b> RClone Config :</b> <i>{RCLONE}</i>
+❒ <b> Mirror Prefix :</b> <code>{MPREFIX}</code>
+❒ <b> Mirror Suffix :</b> <code>{MSUFFIX}</code>
+❒ <b> Mirror Remname :</b> <code>{MREMNAME}</code>
+❒ <b> DDL Server(s) :</b> <i>{DDL_SERVER}</i>
+❒ <b> User TD Mode :</b> <i>{TMODE}</i>
+❒ <b> Total User TD(s) :</b> <i>{USERTD}</i>
+❒ <b> Daily Mirror :</b> <code>{DM}</code> per day'''
 
     LEECH = '''㊂ <b><u>Leech Settings for {NAME}</u></b>
 
-┎<b> Daily Leech : </b><code>{DL}</code> per day
-┠<b> Leech Type :</b> <i>{LTYPE}</i>
-┠<b> Custom Thumbnail :</b> <i>{THUMB}</i>
-┠<b> Leech Split Size :</b> <code>{SPLIT_SIZE}</code>
-┠<b> Equal Splits :</b> <i>{EQUAL_SPLIT}</i>
-┠<b> Media Group :</b> <i>{MEDIA_GROUP}</i>
-┠<b> Leech Caption :</b> <code>{LCAPTION}</code>
-┠<b> Leech Prefix :</b> <code>{LPREFIX}</code>
-┠<b> Leech Suffix :</b> <code>{LSUFFIX}</code>
-┠<b> Leech Dumps :</b> <code>{LDUMP}</code>
-┖<b> Leech Remname :</b> <code>{LREMNAME}</code>'''
+❒ <b> Daily Leech : </b><code>{DL}</code> per day
+❒ <b> Leech Type :</b> <i>{LTYPE}</i>
+❒ <b> Custom Thumbnail :</b> <i>{THUMB}</i>
+❒ <b> Leech Split Size :</b> <code>{SPLIT_SIZE}</code>
+❒ <b> Equal Splits :</b> <i>{EQUAL_SPLIT}</i>
+❒ <b> Media Group :</b> <i>{MEDIA_GROUP}</i>
+❒ <b> Leech Caption :</b> <code>{LCAPTION}</code>
+❒ <b> Leech Prefix :</b> <code>{LPREFIX}</code>
+❒ <b> Leech Suffix :</b> <code>{LSUFFIX}</code>
+❒ <b> Leech Dumps :</b> <code>{LDUMP}</code>
+❒ <b> Leech Remname :</b> <code>{LREMNAME}</code>'''
